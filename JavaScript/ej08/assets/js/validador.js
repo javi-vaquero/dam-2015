fe5e0f1fe816560ec96e7044774d5af7478ee4a0
Javier Vaquero <javi.vaquero@gmail.com>
2015-02-26 00:08:10 +0100

    cambios validador

--- JavaScript/ej08/assets/js/validador.js
+++ JavaScript/ej08/assets/js/validador.js
@@ -1,26 +1,82 @@
-HTMLFormElement.prototype.validate = function() {
+HTMLFormElement.prototype.validate = function(event) {
 
     var validator = {
-    	required: function(){
-    		return false;
-    	};
-    	email: function(){
-    		return false;
-    	};
+    	required: function(el){
+    		return el.value !== '';
+    	},
+    	email: function(el){
+            console.log("email: " + el.value);
+            return /^\w([\w.\-]*\w)?@[A-Za-z0-9]([\w.\-]*\w)?\.[A-Za-z]{2,3}$/.test(el.value);
+    	},
+        passwd: function(el){
+            var b6char = /\w{6,}/.test(el.value);
+            var bMin = /[a-z]/.test(el.value);
+            var bMay = /[A-Z]/.test(el.value);
+            var bDig = /\d/.test(el.value);
+            console.log("b6char: ", b6char);
+            console.log("bMin: ", bMin);
+            console.log("bMay: ", bMay);
+            console.log("bDig: ", bDig);
+            return (b6char && bMin && bMay && bDig);
+        }
     };
 
-    var validate = function() {
-    	var required = this.querySelectorAll(".required");
-    	if(required.length){
-    		for(var i=0; i<required.length){
+    var validate = function(event) {
 
+        event.preventDefault();
+        
+    	var aRequired = this.querySelectorAll(".required");
+        var bRequired = true;
+    	if(aRequired.length){
+    		for(var i=0; i<aRequired.length;i++){
+                if (!validator.required(aRequired[i])){
+                    bRequired=false;
+                    console.log(aRequired[i].name + " es falso");
+                    break;
+                }
     		}
     	}
 
 
-    	var email = this.querySelectorAll(".email");
+    	var aEmail = this.querySelectorAll(".valid-email");                        
+        var bEmail = true;
+        if(aEmail.length){
+            for(i=0; i<aEmail.length;i++){
+                if (!validator.email(aEmail[i])){
+                    bEmail=false;
+                    break;
+                }
+            }
+        }
 
-    	//var password = this.querySelectorAll(input["type=password"]);
+    	var aPassword = this.querySelectorAll("input[type='password']");
+        console.log(aPassword);
+        var bPasswd=true;
+        if(aPassword.length){
+            for(i=0; i<aPassword.length;i++){
+                if (!validator.passwd(aPassword[i])){
+                    bPasswd=false;
+                    break;
+                }
+            }
+        }
+
+        console.log("bRequired: " + bRequired);
+        console.log("bEmail: " + bEmail);
+        console.log("bPasswd: " + bPasswd);
+        if(!(bRequired && bEmail && bPasswd)){
+            event.preventDefault();
+            if(!bRequired){
+                console.log("Hay campos requeridos no rellenados!!");
+            }
+            if(!bEmail){
+                console.log("Hay campos con direccion de email no válidas!!");
+            }
+            if(!bPasswd){
+                console.log("Hay campos de password que no cumplen!!");
+            }
+            
+        }
 
     };
 
